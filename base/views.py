1936e5925396697a68ba040a419b12745f215f25
Ivan <vanechka.miron@bk.ru>
2020-05-27 23:53:05 +0300

    first commit

---
+++ base/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from .models import Dict
+from .forms import GetDict
+import re
+# Create your views here.
+english = re.compile(r"[a-z]", re.I)
+russha = re.compile(r"[а-я]", re.I)
+def index(request):
+    if(request.method =="POST"):
+        gendr = request.POST.get("word").lower()
+        if english.search(gendr):
+            d = Dict.objects.get(englishWord=gendr);
+            return render(request, 'base/content.html', {'d': d })
+        if russha.search(gendr):
+            d = Dict.objects.get(rush=gendr);
+            return render(request, 'base/content.html', {'d': d})
+        
+    else:
+        d = Dict.objects.all()
+        return render(request, 'base/content.html', {'d': d})
+
+def send(request):
+    try:
+        if(request.method == "POST"):
+            data = request.FILES['file'].readlines()
+            import re
+            temOne = re.compile(r"<k>(.*)</k>", re.I | re.S)
+            temTwo = re.compile(r"(.*)</ar>", re.I | re.S)
+            b = 1
+            text = ''
+            for s in data:
+                s = s.decode("utf-8", "ignore")
+                if b == 0 and temOne.search(s):
+                    text = "".join(temOne.findall(s)).lower()
+                    b = 1
+                elif b==1 and temTwo.search(s) :
+                    textTwo = "".join(temTwo.findall(s)).lower()
+                    dict = Dict()
+                    dict.rush = text
+                    dict.englishWord = textTwo
+                    dict.save()
+                    b = 0
+            return redirect("/")
+    except OSError as exc:
+        if exc.errno == 36:
+            return HttpResponse('None')
+        else:
+            raise  # re-raise previously caught exceptions
